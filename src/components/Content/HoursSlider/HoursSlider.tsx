d77aef43364cdc2f22db6ae27e395099cd1d457f
VanyaMate <buhu653@mail.ru>
2023-03-15 20:53:06 +0300

    обновлено отображение hoursSlider mark

--- src/components/Content/HoursSlider/HoursSlider.tsx
+++ src/components/Content/HoursSlider/HoursSlider.tsx
@@ -1,30 +1,39 @@
-import React, {useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import DefaultContainer from "../../UI/Containers/DefaultContainer/DefaultContainer";
 import css from './HoursSlider.module.scss';
-import {IDefaultComponent} from "../../defaultComponent.interface";
-import * as Weather from "../../../store/weather/weather.interfaces";
 import Button from "../../UI/Buttons/Button/Button";
 import {useActions, useMySelector} from "../../../hooks/redux.hooks";
-import CenterTitle from "../../UI/Titles/CenterTitle/CenterTitle";
 
 
 const HoursSlider = React.memo(() => {
     const weather = useMySelector((state) => state.weather);
     const {setCurrentWeatherData} = useActions();
-    const [selectedTime, setSelectedTime] = useState<string>();
+    const [selectedTime, setSelectedTime] = useState<number>();
+
+    const currentTimeMarkPosition = useMemo(() => {
+        const localTime = weather.current?.location.localtime.split(' ')[1].split(':');
+
+        if (localTime) {
+            const [hours, minutes] = localTime;
+            return 75 * (+hours + (100 / 60 * +minutes) / 100) + 35;
+        } else {
+            return 0;
+        }
+    }, [weather.current])
 
     return (
         <DefaultContainer>
             <div className={css.container}>
+                <div className={css.mark} style={{left: currentTimeMarkPosition}}/>
                 {
                     weather.current?.forecast.forecastday[0].hour.map((hourWeather) => {
                         return <Button
                             key={hourWeather.time}
                             active
                             className={css.item}
-                            always={selectedTime === hourWeather.time}
+                            always={selectedTime === hourWeather.time_epoch}
                             onClick={() => {
-                                setSelectedTime(hourWeather.time);
+                                setSelectedTime(hourWeather.time_epoch);
                                 setCurrentWeatherData(hourWeather);
                             }}
                         >
@@ -32,7 +41,7 @@ const HoursSlider = React.memo(() => {
                         </Button>
                     })
                 }
-            </div>
+                </div>
         </DefaultContainer>
     );
 });
